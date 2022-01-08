82f4051a08ebf708fb63e260874ac40bea17bce9
anastasia-pashkova <anastasia.pashkova@gmail.com>
2022-01-07 16:33:55 -0800

    html form

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  #return render_template('index.html') #TODO remove
+  return render_template('form.html')
 
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+@app.route('/get_chart')
+def getchart():
+  # TODO get query params
+  # TODO call API to get data
+  # TODO render chart
+  return "GETTING CHART ..."
 
 if __name__ == '__main__':
   app.run(port=33507)
