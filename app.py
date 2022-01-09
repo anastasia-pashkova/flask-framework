3b8d17c0a9496eafd3b3008c603e9a17e0e1e29d
anastasia-pashkova <anastasia.pashkova@gmail.com>
2022-01-09 13:33:37 -0800

    alpha vantage api call

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect
 import requests
+import os
 #import plotly
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import file_html
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -19,7 +21,25 @@ def about():
 
 @app.route('/fun_api')
 def fun_api():
-  r = requests.get('https://api.github.com/anastasia-pashkova')
+
+  api_key = os.environ.get("AAP_ALPHA_API_KEY")
+  query_params = {
+    'adjusted': True, 
+    'sorat': 'sc', 
+    'limit': '120',
+    'apiKey': api_key
+   }
+
+  ticker = 'AAPL'
+  year = 2021
+  month = '07'
+  days = 30
+  # TODO - for number of days in month and month is 2 digits
+  url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/{year}-{month}-01/2021-{month}-{days}'
+
+  r = requests.get(url,
+    params=query_params
+  )
   return r.json()
 
 
