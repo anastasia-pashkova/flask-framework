05ebd63902ec36a04b35a2eccfaad70b71ff5e17
anastasia-pashkova <anastasia.pashkova@gmail.com>
2022-01-08 19:09:04 -0800

    playing with requests and chart

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, redirect
+import requests
+#import plotly
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import file_html
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -12,12 +17,31 @@ def index():
 def about():
   return render_template('about.html')
 
+@app.route('/fun_api')
+def fun_api():
+  r = requests.get('https://api.github.com/anastasia-pashkova')
+  return r.json()
+
+
+  #return "OK!"
+
+@app.route('/chart_draw')
+def chart_draw():
+  plot = figure()
+  plot.circle([1,2], [3,4])
+  html = file_html(plot, CDN, "my plot")
+  return html
+
 @app.route('/get_chart')
 def getchart():
   # TODO get query params
   # TODO call API to get data
   # TODO render chart
-  return "GETTING CHART ..."
+  # get_chart?tickername=AAPL&year=2021&month=12
+  ticker_name = request.args.get('tickername')
+  year = request.args.get('year')
+  month = request.args.get('month')
+  return f"GETTING CHART for {ticker_name} for year: {year} and month {month}"
 
 if __name__ == '__main__':
   app.run(port=33507)
